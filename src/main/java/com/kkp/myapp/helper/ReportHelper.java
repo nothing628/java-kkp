49d08145d0d44ca97ca972253097f635e7c4f0a7
nothing628 <yogyphang@gmail.com>
2021-07-01 14:59:20 +0700

    remove unused test method

--- src/main/java/com/kkp/myapp/helper/ReportHelper.java
+++ src/main/java/com/kkp/myapp/helper/ReportHelper.java
@@ -85,26 +85,4 @@ public class ReportHelper {
             wb.write(out);
         }
     }
-
-    public void test() {
-        try {
-            // create a new SXSSFWorkbook
-            SXSSFWorkbook wb = new SXSSFWorkbook(100);
-            Sheet sh = wb.createSheet();
-
-            for (int rownum = 0; rownum < 2000; rownum++) {
-                Row row = sh.createRow(rownum);
-                // insert data in 20000 cells
-                for (int cellnum = 0; cellnum < 10; cellnum++) {
-                    Cell cell = row.createCell(cellnum);
-                    cell.setCellValue("Row Number: " + rownum + " Cell Number: " + cellnum);
-                }
-            }
-            try (FileOutputStream out = new FileOutputStream("LargeDcument.xlsx")) {
-                wb.write(out);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(ReportHelper.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 }
